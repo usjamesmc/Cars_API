0c55e5fef0524d4b85db2a779ff6fabcb36dad92
usjamesmc <usjamesmc@gmail.com>
2022-04-13 09:40:05 -0500

    Completed Delete Functionality for Car Views File

--- cars/views.py
+++ cars/views.py
@@ -17,7 +17,7 @@ def cars_list(request):
         serializer.save()
         return Response(serializer.data, status = status.HTTP_201_CREATED)
 
-@api_view(['GET', 'PUT'])       
+@api_view(['GET', 'PUT', 'DELETE'])       
 def car_detail(request, pk):
     car = get_object_or_404(Car, pk = pk)
     if request.method == 'GET':
@@ -28,6 +28,10 @@ def car_detail(request, pk):
        serializer.is_valid(raise_exception = True)
        serializer.save()
        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        car.delete()
+        return Response(status = status.HTTP_204_NO_CONTENT)
+    
 
    
 
